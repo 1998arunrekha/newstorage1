093935cfd244b3a10ce648910717e4cffbeb430b
1998arunrekha <arunar16061998@gmail.com>
2022-09-20 10:43:45 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg1"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet11"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sb" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+resource "azurerm_storage_account" "firststorageaccount11" {
+  name                = "networkstorageaccout"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.sb.id]
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
